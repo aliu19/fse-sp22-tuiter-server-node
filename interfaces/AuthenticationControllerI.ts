31184cf790054b52336d6701fd06ebb4683d7957
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-04-13 01:19:39 -0400

    copy delete method to authController and fix a rare bug

--- interfaces/AuthenticationControllerI.ts
+++ interfaces/AuthenticationControllerI.ts
@@ -8,4 +8,5 @@ export default interface AuthenticationControllerI {
     signup(req: Request, res: Response): void;
     profile(req: Request, res: Response): void;
     logout(req: Request, res: Response): void;
+    delete(req: Request, res: Response): void;
 }
